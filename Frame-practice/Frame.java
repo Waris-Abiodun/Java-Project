183fe61ed7c808954b97682bb1fe8cfb34cead3e
Waris-Abiodun <kazeemwaris05@gmail.com>
2022-09-13 03:21:23 +0100

    update

--- Frame-practice/Frame.java
+++ Frame-practice/Frame.java
@@ -1,22 +1,58 @@
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements ActionListener {
 	Label mylabel = new Label();
 	Pane mypane = new Pane();
+	JButton myButton;       //declare globally here s the actionlistiner can listen to it
+	JLabel iconlabel;
 
 	ImageIcon logo = new ImageIcon("1569775766579.jpg");  //import pics from our library
 	ImageIcon meme = new ImageIcon("2604.jpg");
+	ImageIcon buttonIcon = new ImageIcon("40583.jpg");
+	
 	JPanel yellowPanel = new JPanel();
 	JPanel redPanel = new JPanel();
 	JPanel bluePanel = new JPanel();
 	
+	
 	Frame() {
+		/********************************************************BUtton Practice****************************************************************/
+		
+		 myButton = new JButton();        
+		 myButton.setBounds(200, 100, 215, 50);   //setting the dimensonnand positin
+		 myButton.addActionListener(this);  //this allow our frame to send out the result if te action
+		 //myButton.addActionListener(e -> System.out.println("Bro a new way to write action listener without adding the override function below this constructor")); //eitheer of the two can be use , but i think this will optimise and save time 		 
+		 myButton.setText("Check the magic");
+		 myButton.setFocusable(false);  //it helps to bget rid of the box around text
+		 myButton.setBackground(Color.magenta);
+		 myButton.setFont(new Font("New times roman",Font.ITALIC,12));
+		 myButton.setForeground(Color.YELLOW);
+		 myButton.setBorder(BorderFactory.createEtchedBorder());
+		 myButton.setEnabled(true);   //to enable or disable our button , normally it will always be in tru
+		// myButton.setIcon(logo);
+		 //we can use set horizontal,vertical, alighnment with button ~~~ etc
+		 /***************************************************************************************************************************************/
+		 /******************************************************USing a new Label to display an icon after clicking button*************************************************************/
+		 iconlabel = new JLabel();
+		 iconlabel.setIcon(buttonIcon);
+		 iconlabel.setBounds(200,0, 400,500);
+		 iconlabel.setVisible(false);
+		 
+		 
+		 /*************************************** using JFRame for practice****************************************************/
+		 
 	//	JFrame this = new JFrame(); //create a this
 		this.setVisible(true);     //This will show our this
 		this.setTitle("Kazeem Waris Abiodun");   //the this name shows at the top corner
@@ -30,7 +66,7 @@ public class Frame extends JFrame {
 		this.setLayout(null); //use to manage the layout , for now let set null
 		//this.pack();  //it will pack all our label and automatically give it a size, manage the label so Frame.setsize might not be needed.
 		 //add our components : components are panel , labels in our program
-		
+	/*******************************************using panel for pracice********************************************/
 		yellowPanel.setBackground(Color.yellow);
 		yellowPanel.setBounds(300, 250, 300, 200);
 		yellowPanel.add(mylabel);
@@ -45,13 +81,30 @@ public class Frame extends JFrame {
 		bluePanel.setBounds(0, 250, 300, 200);
 	//	bluePanel.add(mylabel);
 		redPanel.setLayout(null);
+		/*******************************adding component to the frame **********************************************************/
 		
+		this.add(myButton);
+		this.add(iconlabel);
 		this.add(mylabel);
-		this.add(bluePanel);
-		this.add(yellowPanel);
 		
 		this.add(mypane);
+		this.add(bluePanel);
+		this.add(yellowPanel);
 		this.add(redPanel);
+		
+	}
+	
+	/*********************use to write or display the response of action a=button************************************************/ 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == myButton){
+			System.out.println("Bro you just click the button");
+			//myButton.setEnabled(false);   //after first cliked it will stop
+			 iconlabel.setVisible(true);
+			
+			
+			
+		}
 	}
 	private static final long serialVersionUID = 1L;
 }
